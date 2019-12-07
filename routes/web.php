1243107b090be7ff4dc516d7deba94d9d8b0d4ff
guilherme-biscaino <guilhermebs.bs2@hotmail.com>
2019-12-07 03:08:30 -0300

    routes setup

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::deçete('/news/{id}', 'NewsController@destroy')->name('news.destroy');
+
+Route::put('/news/{id}', 'NewsController@update')->name('news.update');
+
+Route::get('/news/{id}/edit','NewsController@edit')->name('news.edit');
+
+Route::get('/news/create',"NewsController@create")->name('news.create');
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/contato', function () {
+    return view('contat');
+});
+Route::get('/news/{id}','NewsController@show')->name('news.show');
+
+Route::get('/news','NewsController@index')->name('news.index');
+
+Route::post('/news','NewsController@store')->name('news.store');
+
+Route::get('/login', function () {
+    return 'login';
 });
